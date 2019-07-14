16f21ffc9ada8c98b8d4c71b03a8dc9de3831511
fallsc <charlie29charlie@gmail.com>
2019-07-14 11:35:11 +0100

    Update main.h

--- DayPlanner/main.h
+++ DayPlanner/main.h
@@ -10,7 +10,18 @@ public:
 		std::cout << MAIN_TEXT << std::endl;
 		//Outputs title of method to user taking into account any number of spaces required to keep everything symmetrical
 		std::cout << NoOfSpaceCompute() << "* Main Menu *" << std::endl;
-
+		//Outputs additional empty line space
+		std::cout << std::endl;
+		//Outputting options to the user
+		std::cout << "1. Schedule a Day" << std::endl;
+		std::cout << "2. View Scheduled Days" << std::endl;
+		std::cout << "3. Quit Application" << std::endl;
+		//Printing additional spacing and asking for user input
+		std::cout << std::endl;
+		std::cout << "Please enter your choice: ";
+		std::cin >> MAIN_MENU_USER_CHOICE;
+		//Executes method to test user input
+		TestMenuUserInput();
 		return;
 	}
 	//Performs task of computing number of spaces required to adjust the method title in conjunction to the main: Day Planner / Scheduler text
@@ -25,11 +36,18 @@ public:
 		//Method returns the string consisting of only spaces
 		return ADDED_SPACES;
 	}
+	void TestMenuUserInput()
+	{
+
+		return;
+	}
 private:
 	//constant string of the program's main text that will be outputted regardless of the method executed
 	const std::string MAIN_TEXT = "* Day Planner / Scheduler *";
 	//constant integer equalling the number of characters present within the main text string divided by four
-	const int NUMBER_OF_CHAR = (MAIN_TEXT.size()/4);
+	const int NUMBER_OF_CHAR = (MAIN_TEXT.size()/DIVISOR_NUM_CHAR);
+	const int DIVISOR_NUM_CHAR = 4;
 	//initialising the added spaces text as empty
 	std::string ADDED_SPACES = "";
+	std::string MAIN_MENU_USER_CHOICE = "";
 };
