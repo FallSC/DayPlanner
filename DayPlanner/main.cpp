d8aed0c44b145ef95d22fdc2ba983f037a79f0f6
fallsc <charlie29charlie@gmail.com>
2019-07-14 11:02:46 +0100

    Add project files.

---
+++ DayPlanner/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "main.h"
+
+int main()
+{
+	//Initialise class which performs every action within the program
+	Planner PlanInit;
+
+	//Execute main menu
+	PlanInit.MainMenu();
+
+	//Returns nil
+	return 0;
+}
---
+++ DayPlanner/main.h
@@ -0,0 +1,32 @@
+#pragma once
+
+//Planner class holds all necessary functions the program needs to carry out tasks
+class Planner {
+public:
+	//Creating main menu function of class, handles output and taking of input from the user so that they can move to different parts of the program
+	void MainMenu()
+	{
+		//Outputs the main text: Day Planner / Scheduler, this is done regardless of the function executed and persists throughout the program
+		std::cout << MAIN_TEXT << std::endl;
+		//Outputs title of method to user taking into account any number of spaces required to keep everything symmetrical
+		std::cout << NoOfSpaceCompute() << "* Main Menu *" << std::endl;
+
+		return;
+	}
+	//Performs task of computing number of spaces required to adjust the method title in conjunction to the main: Day Planner / Scheduler text
+	std::string NoOfSpaceCompute()
+	{
+		//Runs a loop - adjusting the number of spaces string based on how many character the main text consists of
+		for (int i = 0; i < NUMBER_OF_CHAR; i++)
+		{
+			//Modifies string with a space each time
+			ADDED_SPACES.append(" ");
+		}
+		//Method returns the string consisting of only spaces
+		return ADDED_SPACES;
+	}
+private:
+	const std::string MAIN_TEXT = "* Day Planner / Scheduler *";
+	const int NUMBER_OF_CHAR = (MAIN_TEXT.size()/4);
+	std::string ADDED_SPACES = "";
+};
